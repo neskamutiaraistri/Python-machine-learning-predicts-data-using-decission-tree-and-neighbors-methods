a16539059fdfebd29009cb8ddbe55d8b984fde2d
neskamutiaraistri <114650026+neskamutiaraistri@users.noreply.github.com>
2022-11-24 21:39:24 +0700

    Menambahkan File

---
+++ Metode Decision Tree 1.py
@@ -0,0 +1,24 @@
+from sklearn import tree
+
+# Database: Gerbang logika AND
+# x = Data, Y = Target
+x = [[0,0,0], [0,0,1], [0,1,0], [1,0,0], [1,1,1], [1,2,2], [1,1,2], [1,2,1], [2,1,1], [2,2,2]]
+y = [0,0,0,0,0,2,1,1,1,0]
+
+# Training and Classify
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x,y)
+
+# Prediksi
+print ("Logika AND Metode Decision Tree")
+print ("Logika = Prediksi")
+print ("0 0 0 = ", clf.predict([[0,0,0]]))
+print ("0 0 1 = ", clf.predict([[0,0,1]]))
+print ("0 1 0 = ", clf.predict([[0,1,0]]))
+print ("1 0 0 = ", clf.predict([[1,0,0]]))
+print ("1 1 1 = ", clf.predict([[1,1,1]]))
+print ("1 2 2 = ", clf.predict([[1,2,2]]))
+print ("1 1 2 = ", clf.predict([[1,1,2]]))
+print ("1 2 1 = ", clf.predict([[1,2,1]]))
+print ("2 1 1 = ", clf.predict([[2,1,1]]))
+print ("2 2 2 = ", clf.predict([[2,2,2]]))
